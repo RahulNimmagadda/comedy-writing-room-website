b1763af2a02d62f7124f45b7a85bbb9afcaeefcf
Rahul Nimmagadda <YOUR_GITHUB_EMAIL>
2026-02-17 19:09:57 -0500

    Fix Stripe apiVersion type error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-});
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing STRIPE_SECRET_KEY");
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {});
