20520e318abfa59908052da908ac8963fc192208
Rahul Nimmagadda <YOUR_GITHUB_EMAIL>
2026-02-25 20:39:47 -0500

    Fix TimezoneField to avoid setState-in-effect lint error

--- components/TimezoneField.tsx
+++ components/TimezoneField.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 export default function TimezoneField({
   name = "timezone",
 }: {
   name?: string;
 }) {
-  const [tz, setTz] = useState<string>("");
-
-  useEffect(() => {
-    try {
-      const z = Intl.DateTimeFormat().resolvedOptions().timeZone;
-      if (z) setTz(z);
-    } catch {
-      // ignore
-    }
-  }, []);
+  let tz = "";
+  try {
+    tz = Intl.DateTimeFormat().resolvedOptions().timeZone || "";
+  } catch {
+    tz = "";
+  }
 
   return <input type="hidden" name={name} value={tz} />;
 }
